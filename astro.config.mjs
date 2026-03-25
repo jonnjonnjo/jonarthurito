1f4a4e98ab3fe28f4295c42560f4a151bf5e5491
jonnjonnjo <zhenagajo234@gmail.com>
2026-03-25 11:32:02 +0700

    fix: fix styling of each blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro:content'; // or 'astro/config'
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
-  integrations: [mdx()],
-  markdown: {
-    remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
-  },
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
