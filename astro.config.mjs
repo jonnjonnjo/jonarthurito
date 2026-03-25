47ace0653657e15f09d47ffafe629513e0cee220
jonnjonnjo <zhenagajo234@gmail.com>
2026-03-25 15:04:26 +0700

    fix: fix import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro:content'; // or 'astro/config'
+import { defineConfig } from 'astro/config'; // or 'astro/config'
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 import remarkMath from 'remark-math';
